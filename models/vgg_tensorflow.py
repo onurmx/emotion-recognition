ac2ae23ad2db1fbe27d0da07468a0446220411d7
Onur Caglayan <onurcag99@gmail.com>
2022-07-03 07:20:25 +0200

    VGG TENSORFLOW

---
+++ models/vgg_tensorflow.py
@@ -0,0 +1,51 @@
+import tensorflow as tf
+
+class Vgg:
+    def build_model():
+        # input
+        i = tf.keras.layers.Input(shape=(224, 224, 3))
+        
+        # block 1
+        x = tf.keras.layers.Conv2D(64, (3, 3), activation='relu', padding='same', name='block1_conv1')(i)
+        x = tf.keras.layers.Conv2D(64, (3, 3), activation='relu', padding='same', name='block1_conv2')(x)
+        x = tf.keras.layers.MaxPooling2D((2, 2), strides=(2, 2), name='block1_pool')(x)
+
+        # block 2
+        x = tf.keras.layers.Conv2D(128, (3, 3), activation='relu', padding='same', name='block2_conv1')(x)
+        x = tf.keras.layers.Conv2D(128, (3, 3), activation='relu', padding='same', name='block2_conv2')(x)
+        x = tf.keras.layers.MaxPooling2D((2, 2), strides=(2, 2), name='block2_pool')(x)
+
+        # block 3
+        x = tf.keras.layers.Conv2D(256, (3, 3), activation='relu', padding='same', name='block3_conv1')(x)
+        x = tf.keras.layers.Conv2D(256, (3, 3), activation='relu', padding='same', name='block3_conv2')(x)
+        x = tf.keras.layers.Conv2D(256, (3, 3), activation='relu', padding='same', name='block3_conv3')(x)
+        x = tf.keras.layers.MaxPooling2D((2, 2), strides=(2, 2), name='block3_pool')(x)
+
+        # block 4
+        x = tf.keras.layers.Conv2D(512, (3, 3), activation='relu', padding='same', name='block4_conv1')(x)
+        x = tf.keras.layers.Conv2D(512, (3, 3), activation='relu', padding='same', name='block4_conv2')(x)
+        x = tf.keras.layers.Conv2D(512, (3, 3), activation='relu', padding='same', name='block4_conv3')(x)
+        x = tf.keras.layers.MaxPooling2D((2, 2), strides=(2, 2), name='block4_pool')(x)
+
+        # block 5
+        x = tf.keras.layers.Conv2D(512, (3, 3), activation='relu', padding='same', name='block5_conv1')(x)
+        x = tf.keras.layers.Conv2D(512, (3, 3), activation='relu', padding='same', name='block5_conv2')(x)
+        x = tf.keras.layers.Conv2D(512, (3, 3), activation='relu', padding='same', name='block5_conv3')(x)
+        x = tf.keras.layers.MaxPooling2D((2, 2), strides=(2, 2), name='block5_pool')(x)
+
+        # fully connected layer
+        x = tf.keras.layers.Flatten(name='flatten')(x)
+        x = tf.keras.layers.Dense(4096, activation='relu', name='fc1')(x)
+        x = tf.keras.layers.Dense(4096, activation='relu', name='fc2')(x)
+        x = tf.keras.layers.Dense(1000, activation='softmax', name='predictions')(x)
+
+        model = tf.keras.Model(i, x)
+        return model
+    
+    def compile_model(model):
+        model.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['accuracy'])
+        return model
+    
+    def train_model(model, train_data, train_labels, test_data, test_labels, epochs=10):
+        model.fit(train_data, train_labels, epochs=epochs, validation_data=(test_data, test_labels))
+        return model
