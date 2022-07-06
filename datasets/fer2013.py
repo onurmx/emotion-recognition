7b82accd7898df7d8d1c82ca126c8aba80297afa
Onur Caglayan <onurcag99@gmail.com>
2022-07-06 06:21:21 +0200

    load_fer2013 optimization

--- datasets/fer2013.py
+++ datasets/fer2013.py
@@ -1,30 +1,28 @@
 import pandas as pd
 import numpy as np
-import cv2 as cv
 
 def load_fer2013(filepath):
     # read data
     df = pd.read_csv(filepath)
 
-    # partition data into training, and test sets
-    df_train = df[df['Usage'] == 'Training']
-    df_test = df[df['Usage'] == 'PublicTest']
-
     # get pixels
-    x_train = df_train['pixels']
-    x_test = df_test['pixels']
+    x_train = df[df['Usage'] == 'Training']['pixels']
+    x_test = df[df['Usage'] == 'PublicTest']['pixels']
 
     # get labels
-    y_train = df_train['emotion']
-    y_test = df_test['emotion']
+    y_train = df[df['Usage'] == 'Training']['emotion']
+    y_test = df[df['Usage'] == 'PublicTest']['emotion']
+
+    # destroy dataframe
+    del df
 
     # split pixels
     x_train = np.array([np.fromstring(x, sep=' ') for x in x_train])
     x_test = np.array([np.fromstring(x, sep=' ') for x in x_test])
 
     # reshape pixels
-    x_train = x_train.reshape(len(df_train), 48, 48)
-    x_test = x_test.reshape(len(df_test), 48, 48)
+    x_train = x_train.reshape(len(x_train), 48, 48)
+    x_test = x_test.reshape(len(x_test), 48, 48)
 
     # normalize pixels
     x_train /= 255
@@ -34,8 +32,8 @@ def load_fer2013(filepath):
     x_train = np.stack((x_train,)*3, axis=-1)
     x_test = np.stack((x_test,)*3, axis=-1)
 
-    # resize images into 224x224
-    x_train = np.array([cv.resize(x, (224, 224)) for x in x_train])
-    x_test = np.array([cv.resize(x, (224, 224)) for x in x_test])
+    # upsample images into 240x240
+    x_train = np.array([x.repeat(5, axis=0).repeat(5, axis=1) for x in x_train])
+    x_test = np.array([x.repeat(5, axis=0).repeat(5, axis=1) for x in x_test])
 
     return x_train, y_train, x_test, y_test
