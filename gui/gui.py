df52ef46d7dd917a0836ac809e1dfc7e9e896472
Onur Caglayan <onurcag99@gmail.com>
2022-07-24 10:52:01 +0200

    asd

--- gui/gui.py
+++ gui/gui.py
@@ -4,5 +4,13 @@ class EmotionRecognitionApplication(tk.Frame):
     def __init__(self, parent, *args, **kwargs):
         tk.Frame.__init__(self, parent, *args, **kwargs)
         self.parent = parent
-        self.parent.geometry("500x500")
-        self.parent.title("Emotion Recognition Application")
+
+        # center the window
+        width = 900
+        height = 750
+        x = (self.parent.winfo_screenwidth() // 2) - (width // 2)
+        y = (self.parent.winfo_screenheight() // 2) - (height // 2)
+        self.parent.geometry('{}x{}+{}+{}'.format(width, height, x, y))
+
+        # set the title
+        self.parent.title("Emotion Recognition")
