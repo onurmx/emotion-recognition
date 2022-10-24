7870c091277315db20d7575b2fea29cdc326c33b
Onur Caglayan <onurcag99@gmail.com>
2022-10-24 22:14:41 +0200

    fer2013 tf improvement

--- datasets/fer2013/tf_fer2013.py
+++ datasets/fer2013/tf_fer2013.py
@@ -2,41 +2,39 @@ import tensorflow as tf
 import pandas as pd
 import numpy as np
 
-def tf_load_fer2013(filepath, convert_to_rgb = False, upsample = None, batch_size=128):
-    # read data
+def tf_load_fer2013(filepath, convert_to_rgb = False, upsample = None, batch_size=64):
     df = pd.read_csv(filepath)
 
-    # get pixels
     x_train = df[df['Usage'] == 'Training']['pixels']
-    x_test = df[df['Usage'] == 'PublicTest']['pixels']
+    x_val = df[df['Usage'] == 'PublicTest']['pixels']
+    x_test = df[df['Usage'] == 'PrivateTest']['pixels']
 
-    # get labels
     y_train = df[df['Usage'] == 'Training']['emotion'].to_numpy()
-    y_test = df[df['Usage'] == 'PublicTest']['emotion'].to_numpy()
+    y_val = df[df['Usage'] == 'PublicTest']['emotion'].to_numpy()
+    y_test = df[df['Usage'] == 'PrivateTest']['emotion'].to_numpy()
 
-    # split pixels
     x_train = np.array([np.fromstring(x, sep=' ') for x in x_train])
+    x_val = np.array([np.fromstring(x, sep=' ') for x in x_val])
     x_test = np.array([np.fromstring(x, sep=' ') for x in x_test])
 
-    # reshape pixels
     x_train = x_train.reshape(len(x_train), 48, 48)
+    x_val = x_val.reshape(len(x_val), 48, 48)
     x_test = x_test.reshape(len(x_test), 48, 48)
 
-    # convert pixels into rgb
     if convert_to_rgb:
         x_train = np.stack((x_train,)*3, axis=-1)
+        x_val = np.stack((x_val,)*3, axis=-1)
         x_test = np.stack((x_test,)*3, axis=-1)
 
-    # upsample images by upsample factor
     if upsample:
         x_train = np.array([x.repeat(upsample, axis=0).repeat(upsample, axis=1) for x in x_train])
+        x_val = np.array([x.repeat(upsample, axis=0).repeat(upsample, axis=1) for x in x_val])
         x_test = np.array([x.repeat(upsample, axis=0).repeat(upsample, axis=1) for x in x_test])
 
-    # calculate steps per epoch and validation steps
     steps_per_epoch = len(x_train) // batch_size
-    validation_steps = len(x_test) // batch_size
+    validation_steps = len(x_val) // batch_size
+    test_steps = len(x_test) // batch_size
 
-    # Data augmentation using Image Data Generator for train data
     train_generator = tf.keras.preprocessing.image.ImageDataGenerator(
         rotation_range=20,
         width_shift_range=0.2,
@@ -55,7 +53,17 @@ def tf_load_fer2013(filepath, convert_to_rgb = False, upsample = None, batch_siz
         shuffle=True
     )
 
-    # Data augmentation using Image Data Generator for test data
+    validation_generator = tf.keras.preprocessing.image.ImageDataGenerator(
+        rescale=1. / 255
+    )
+
+    validation_data = validation_generator.flow(
+        x_val,
+        y_val,
+        batch_size=batch_size,
+        shuffle=True
+    )
+
     test_generator = tf.keras.preprocessing.image.ImageDataGenerator(
         rescale=1. / 255
     )
@@ -67,4 +75,4 @@ def tf_load_fer2013(filepath, convert_to_rgb = False, upsample = None, batch_siz
         shuffle=True
     )
 
-    return training_data, testing_data, steps_per_epoch, validation_steps
+    return training_data, validation_data, testing_data, steps_per_epoch, validation_steps, test_steps
