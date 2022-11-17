a99999574e7688b17fcae12371880f549c29ee52
Onur Caglayan <onurcag99@gmail.com>
2022-11-17 20:25:22 +0100

    prediction

--- app/app_utils.py
+++ app/app_utils.py
@@ -57,47 +57,35 @@ def prediction_generator(image, backend, model, dataset, workdir):
             elif dataset == "KDEF": 
                 return NotImplementedError
     elif backend == "PyTorch":
-        if model == "Resnet":
+        if model=="Resnet" or model == "VGG":
+            target_image = cv2.resize(image, (224, 224))
+            target_image = torchvision.transforms.ToPILImage()(target_image)
+            target_image = torchvision.transforms.ToTensor()(target_image)
+            model = resnet_pytorch.resnet50(img_channels=3, num_classes=7) if model=="Resnet" else vgg_pytorch.VGG16(num_classes=7)
             if dataset == "FER2013":
-                model = resnet_pytorch.resnet50(img_channels=3, num_classes=7)
-                model.load_state_dict(torch.load(workdir + "/trained_models/" + backend.lower() + "/" + model.lower() + "/" + dataset.lower() + "/model.pt", map_location=torch.device('cpu')))
-                model.eval()
-                target_image = cv2.resize(image, (224, 224))
-                target_image = torchvision.transforms.ToPILImage()(target_image)
-                target_image = torchvision.transforms.ToTensor()(target_image)
-                prediction = model(target_image.unsqueeze(0))
-                emotion_label = torch.argmax(prediction)
-                emotion_label = emotion_label.item()
-                emotion_label = fer2013_label_translator(emotion_label)
-                return emotion_label
+                model.load_state_dict(torch.load(workdir + "/trained_models/pytorch/" + ("resnet" if model == "Resnet" else "vgg") + "/fer2013/model.pt", map_location=torch.device('cpu')))
             elif dataset == "CK+":
-                return NotImplementedError
+                model.load_state_dict(torch.load(workdir + "/trained_models/pytorch/" + ("resnet" if model == "Resnet" else "vgg") + "/fer2013/model.pt", map_location=torch.device('cpu')))
             elif dataset == "KDEF": 
-                return NotImplementedError
-        elif model == "VGG":
-            if dataset == "FER2013":
-                model = vgg_pytorch.VGG16(num_classes=7)
-                model.load_state_dict(torch.load(workdir + "/trained_models/pytorch/vgg/fer2013/model.pt", map_location=torch.device('cpu')))
-                model.eval()
-                target_image = cv2.resize(image, (224, 224))
-                target_image = torchvision.transforms.ToPILImage()(target_image)
-                target_image = torchvision.transforms.ToTensor()(target_image)
-                prediction = model(target_image.unsqueeze(0))
-                emotion_label = torch.argmax(prediction)
-                emotion_label = emotion_label.item()
-                emotion_label = fer2013_label_translator(emotion_label)
-                return emotion_label
-            elif dataset == "CK+":
-                return NotImplementedError
-            elif dataset == "KDEF": 
-                return NotImplementedError
+                model.load_state_dict(torch.load(workdir + "/trained_models/pytorch/" + ("resnet" if model == "Resnet" else "vgg") + "/fer2013/model.pt", map_location=torch.device('cpu')))
         elif model == "Onsunet":
+            target_image = cv2.resize(image, (48, 48))
+            target_image = torchvision.transforms.ToPILImage()(target_image)
+            target_image = torchvision.transforms.Grayscale(num_output_channels=1)(target_image)
+            target_image = torchvision.transforms.ToTensor()(target_image)
+            model = onsunet_pytorch.Onsunet(num_classes=7)
             if dataset == "FER2013":
-                return NotImplementedError
+                model.load_state_dict(torch.load(workdir + "/trained_models/pytorch/onsunet/fer2013/model.pt", map_location=torch.device('cpu')))
             elif dataset == "CK+":
-                return NotImplementedError
+                model.load_state_dict(torch.load(workdir + "/trained_models/pytorch/onsunet/fer2013/model.pt", map_location=torch.device('cpu')))
             elif dataset == "KDEF": 
-                return NotImplementedError
+                model.load_state_dict(torch.load(workdir + "/trained_models/pytorch/onsunet/fer2013/model.pt", map_location=torch.device('cpu')))
+        model.eval()
+        prediction = model(target_image.unsqueeze(0))
+        emotion_label = torch.argmax(prediction)
+        emotion_label = emotion_label.item()
+        emotion_label = fer2013_label_translator(emotion_label)
+        return emotion_label
 
 def fer2013_label_translator(emotion_label):
     if emotion_label == 0:
