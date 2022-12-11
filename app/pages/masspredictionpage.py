4bef8a38445e56119faffba0fe97ab7a803e9065
Onur Caglayan <onurcag99@gmail.com>
2022-12-11 17:52:59 +0100

    mass prediction logic implemented with threading error

--- app/pages/masspredictionpage.py
+++ app/pages/masspredictionpage.py
@@ -1,5 +1,15 @@
+import app_utils as au
+import cv2
+import os
+import sys
+import traceback
+
 from PySide2.QtCore import (
-    QPoint
+    QPoint,
+    QObject,
+    Signal,
+    Slot,
+    QThread
 )
 from PySide2.QtWidgets import (
     QPushButton,
@@ -71,8 +81,11 @@ class MassPredictionPage(QWidget):
         self.button_predict.setParent(self)
         self.button_predict.setFixedSize(200, 100)
         self.button_predict.move(QPoint(2 * self.parent().size().width() / 3 - self.button_predict.size().width() / 2, 550))
-        self.button_predict.clicked.connect(self.predict)
+        self.button_predict.clicked.connect(self.start_thread)
         self.button_predict.setStyleSheet("font-size: 20px;")
+
+        self.prediction_thread = MassPredictionThread(self.prediction_worker_procedure)
+        self.prediction_thread.signals.finished.connect(self.prediction_worker_finished)
 
         self.is_coming_from_train_page = False
 
@@ -85,5 +98,68 @@ class MassPredictionPage(QWidget):
     def back_page(self):
         self.parent().show_page(self.parent().single_or_mass_prediction_page)
 
-    def predict(self):
-        return NotImplementedError
+    def start_thread(self):
+        self.button_back.setEnabled(False)
+        self.button_predict.setEnabled(False)
+
+        self.prediction_thread.start()
+
+    def prediction_worker_procedure(self):
+        #create a single file for output
+        if self.path_to_save_predictions.text() != "":
+            output_file = open(os.path.join(self.path_to_save_predictions.text(), "predictions.csv"), "w")
+
+        images = [os.path.join(self.path_to_images.text(), f) for f in os.listdir(self.path_to_images.text()) if os.path.isfile(os.path.join(self.path_to_images.text(), f))]
+        images = [f for f in images if f.endswith(".jpg") or f.endswith(".png")]
+
+        for image_path in images:
+            image = cv2.imread(image_path)
+            faces = au.get_faces(image, self.parent().workdir)
+            if len(faces) > 0:
+                backend = self.parent().load_model_page.backend_combobox.currentText().lower() if self.is_coming_from_train_page == False else self.parent().train_model_page.backend_combobox.currentText().lower()
+                model = self.parent().load_model_page.model_combobox.currentText().lower() if self.is_coming_from_train_page == False else self.parent().train_model_page.model_combobox.currentText().lower()
+                dataset = self.parent().load_model_page.dataset_combobox.currentText().lower() if self.is_coming_from_train_page == False else self.parent().train_model_page.dataset_combobox.currentText().lower()
+                workdir = self.parent().workdir
+                emotions = [au.prediction_generator(image[y:y+h, x:x+w],backend, model, ("ckplus" if dataset =="ck+" else dataset),self.parent().train_model_page.trained_net, workdir) for (x, y, w, h) in faces]
+                for emotion in emotions:
+                    if self.path_to_save_predictions.text() != "":
+                        output_file.write(image_path + "," + emotion + "\n")
+
+        if self.path_to_save_predictions.text() != "":
+            output_file.close()
+
+        return 1
+
+    def prediction_worker_finished(self):
+        self.prediction_thread.quit()
+        self.prediction_thread.wait()
+
+        self.button_back.setEnabled(True)
+        self.button_predict.setEnabled(True)
+
+class MassPredictionThreadSignals(QObject):
+    finished = Signal()
+    error = Signal(tuple)
+    result = Signal(object)
+    progress = Signal(int)
+
+class MassPredictionThread(QThread):
+    def __init__(self, fn, *args, **kwargs):
+        super(MassPredictionThread, self).__init__()
+        self.fn = fn
+        self.args = args
+        self.kwargs = kwargs
+        self.signals = MassPredictionThreadSignals()
+
+    @Slot()
+    def run(self):
+        try:
+            result = self.fn(*self.args, **self.kwargs)
+        except:
+            traceback.print_exc()
+            exctype, value = sys.exc_info()[:2]
+            self.signals.error.emit((exctype, value, traceback.format_exc()))
+        else:
+            self.signals.result.emit(result)
+        finally:
+            self.signals.finished.emit()
