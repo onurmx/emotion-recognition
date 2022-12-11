f740800c1d71b7154aa4c0c74e18dcbb329c7188
Onur Caglayan <onurcag99@gmail.com>
2022-12-11 17:02:25 +0100

    mass prediction gui adjustment

--- app/pages/masspredictionpage.py
+++ app/pages/masspredictionpage.py
@@ -14,17 +14,17 @@ class MassPredictionPage(QWidget):
     def __init__(self, parent=None):
         super().__init__(parent)
 
-        self.label = QLabel("Mass Prediction")
-        self.label.setParent(self)
-        self.label.move(QPoint(self.parent().size().width() / 2 - self.label.size().width() / 2, 50))
-        self.label.setStyleSheet("font-size: 30px;")
-        self.label.setFixedSize(400, 50)
+        self.label_mass_prediction = QLabel("Mass Prediction")
+        self.label_mass_prediction.setParent(self)
+        self.label_mass_prediction.move(QPoint(self.parent().size().width() / 2 - self.label_mass_prediction.size().width() / 2, 50))
+        self.label_mass_prediction.setStyleSheet("font-size: 30px;")
+        self.label_mass_prediction.setFixedSize(400, 50)
 
-        self.label = QLabel("Path to images:")
-        self.label.setParent(self)
-        self.label.move(QPoint(self.parent().size().width() / 2 - self.label.size().width() / 2, 150))
-        self.label.setStyleSheet("font-size: 20px;")
-        self.label.setFixedSize(400, 50)
+        self.label_path_to_images = QLabel("Path to images:")
+        self.label_path_to_images.setParent(self)
+        self.label_path_to_images.move(QPoint(self.parent().size().width() / 2 - self.label_path_to_images.size().width() / 2, 150))
+        self.label_path_to_images.setStyleSheet("font-size: 20px;")
+        self.label_path_to_images.setFixedSize(400, 50)
 
         self.path_to_images = QLineEdit()
         self.path_to_images.setParent(self)
@@ -33,19 +33,18 @@ class MassPredictionPage(QWidget):
         self.path_to_images.setStyleSheet("font-size: 20px;")
         self.path_to_images.setReadOnly(True)
 
-        self.button_browse = QPushButton("Browse")
-        self.button_browse.setParent(self)
-        self.button_browse.setFixedSize(200, 50)
-        self.button_browse.move(QPoint(self.parent().size().width() / 2 - self.button_browse.size().width() / 2, 275))
-        self.button_browse.clicked.connect(self.browse)
-        self.button_browse.setStyleSheet("font-size: 20px;")
-        self.button_browse.setDisabled(True)
+        self.button_browse_path_to_images = QPushButton("Browse source path")
+        self.button_browse_path_to_images.setParent(self)
+        self.button_browse_path_to_images.setFixedSize(200, 50)
+        self.button_browse_path_to_images.move(QPoint(self.parent().size().width() / 2 - self.button_browse_path_to_images.size().width() / 2, 275))
+        self.button_browse_path_to_images.clicked.connect(self.browse)
+        self.button_browse_path_to_images.setStyleSheet("font-size: 20px;")
 
-        self.label = QLabel("Path to save predictions:")
-        self.label.setParent(self)
-        self.label.move(QPoint(self.parent().size().width() / 2 - self.label.size().width() / 2, 350))
-        self.label.setStyleSheet("font-size: 20px;")
-        self.label.setFixedSize(400, 50)
+        self.label_path_to_save_predictions = QLabel("Path to save predictions:")
+        self.label_path_to_save_predictions.setParent(self)
+        self.label_path_to_save_predictions.move(QPoint(self.parent().size().width() / 2 - self.label_path_to_save_predictions.size().width() / 2, 350))
+        self.label_path_to_save_predictions.setStyleSheet("font-size: 20px;")
+        self.label_path_to_save_predictions.setFixedSize(400, 50)
 
         self.path_to_save_predictions = QLineEdit()
         self.path_to_save_predictions.setParent(self)
@@ -54,13 +53,12 @@ class MassPredictionPage(QWidget):
         self.path_to_save_predictions.setStyleSheet("font-size: 20px;")
         self.path_to_save_predictions.setReadOnly(True)
 
-        self.button_browse = QPushButton("Browse")
-        self.button_browse.setParent(self)
-        self.button_browse.setFixedSize(200, 50)
-        self.button_browse.move(QPoint(self.parent().size().width() / 2 - self.button_browse.size().width() / 2, 475))
-        self.button_browse.clicked.connect(self.browse)
-        self.button_browse.setStyleSheet("font-size: 20px;")
-        self.button_browse.setDisabled(True)
+        self.button_browse_path_to_save_predictions = QPushButton("Browse output path")
+        self.button_browse_path_to_save_predictions.setParent(self)
+        self.button_browse_path_to_save_predictions.setFixedSize(200, 50)
+        self.button_browse_path_to_save_predictions.move(QPoint(self.parent().size().width() / 2 - self.button_browse_path_to_save_predictions.size().width() / 2, 475))
+        self.button_browse_path_to_save_predictions.clicked.connect(self.browse)
+        self.button_browse_path_to_save_predictions.setStyleSheet("font-size: 20px;")
 
         self.button_back = QPushButton("Back")
         self.button_back.setParent(self)
@@ -79,7 +77,7 @@ class MassPredictionPage(QWidget):
         self.is_coming_from_train_page = False
 
     def browse(self):
-        if self.sender().text() == "Browse":
+        if self.sender().text() == "Browse source path":
             self.path_to_images.setText(QFileDialog.getExistingDirectory(self, "Select Directory"))
         else:
             self.path_to_save_predictions.setText(QFileDialog.getExistingDirectory(self, "Select Directory"))
