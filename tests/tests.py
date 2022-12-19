2faceb14bee5ca780dc40d5c17dd099cfaa32a7e
Onur Caglayan <onurcag99@gmail.com>
2022-12-19 20:49:59 +0100

    test

--- tests/tests.py
+++ tests/tests.py
@@ -6,7 +6,7 @@ import unittest
 class PytorchDatasetTest(unittest.TestCase):
     workdir = "D:/emo/appfiles"
 
-    def test_dataset_ckplus_p1(self):
+    def test_dataset_ckplus_48_1x1_p1(self):
         from datasets.pytorch import ckplus as ckplus_pytorch
         from utils.pytorch import device_management as dm
         train_dl, _2, _3 = ckplus_pytorch.load_ckplus(
@@ -19,7 +19,7 @@ class PytorchDatasetTest(unittest.TestCase):
 
         self.assertEqual(type(train_dl).__name__, "DeviceDataLoader")
 
-    def test_dataset_ckplus_p2(self):
+    def test_dataset_ckplus_48_1x1_p2(self):
         from datasets.pytorch import ckplus as ckplus_pytorch
         from utils.pytorch import device_management as dm
         _1, valid_dl, _3 = ckplus_pytorch.load_ckplus(
@@ -32,7 +32,7 @@ class PytorchDatasetTest(unittest.TestCase):
 
         self.assertEqual(type(valid_dl).__name__, "DeviceDataLoader")
 
-    def test_dataset_ckplus_p3(self):
+    def test_dataset_ckplus_48_1x1_p3(self):
         from datasets.pytorch import ckplus as ckplus_pytorch
         from utils.pytorch import device_management as dm
         _1, _2, test_dl = ckplus_pytorch.load_ckplus(
@@ -45,7 +45,7 @@ class PytorchDatasetTest(unittest.TestCase):
 
         self.assertEqual(type(test_dl).__name__, "DeviceDataLoader")
 
-    def test_dataset_fer2013_p1(self):
+    def test_dataset_fer2013_48_1x1_p1(self):
         from datasets.pytorch import fer2013 as fer2013_pytorch
         from utils.pytorch import device_management as dm
         train_dl, _2, _3 = fer2013_pytorch.load_fer2013(
@@ -58,7 +58,7 @@ class PytorchDatasetTest(unittest.TestCase):
 
         self.assertEqual(type(train_dl).__name__, "DeviceDataLoader")
 
-    def test_dataset_fer2013_p2(self):
+    def test_dataset_fer2013_48_1x1_p2(self):
         from datasets.pytorch import fer2013 as fer2013_pytorch
         from utils.pytorch import device_management as dm
         _1, valid_dl, _3 = fer2013_pytorch.load_fer2013(
@@ -71,7 +71,7 @@ class PytorchDatasetTest(unittest.TestCase):
 
         self.assertEqual(type(valid_dl).__name__, "DeviceDataLoader")
 
-    def test_dataset_fer2013_p3(self):
+    def test_dataset_fer2013_48_1x1_p3(self):
         from datasets.pytorch import fer2013 as fer2013_pytorch
         from utils.pytorch import device_management as dm
         _1, _2, test_dl = fer2013_pytorch.load_fer2013(
@@ -84,7 +84,7 @@ class PytorchDatasetTest(unittest.TestCase):
 
         self.assertEqual(type(test_dl).__name__, "DeviceDataLoader")
 
-    def test_dataset_kdef_p1(self):
+    def test_dataset_kdef_48_1x1_p1(self):
         from datasets.pytorch import kdef as kdef_pytorch
         from utils.pytorch import device_management as dm
         train_dl, _2, _3 = kdef_pytorch.load_kdef(
@@ -97,7 +97,7 @@ class PytorchDatasetTest(unittest.TestCase):
 
         self.assertEqual(type(train_dl).__name__, "DeviceDataLoader")
 
-    def test_dataset_kdef_p2(self):
+    def test_dataset_kdef_48_1x1_p2(self):
         from datasets.pytorch import kdef as kdef_pytorch
         from utils.pytorch import device_management as dm
         _1, valid_dl, _3 = kdef_pytorch.load_kdef(
@@ -110,7 +110,7 @@ class PytorchDatasetTest(unittest.TestCase):
 
         self.assertEqual(type(valid_dl).__name__, "DeviceDataLoader")
 
-    def test_dataset_kdef_p3(self):
+    def test_dataset_kdef_48_1x1_p3(self):
         from datasets.pytorch import kdef as kdef_pytorch
         from utils.pytorch import device_management as dm
         _1, _2, test_dl = kdef_pytorch.load_kdef(
@@ -123,10 +123,127 @@ class PytorchDatasetTest(unittest.TestCase):
 
         self.assertEqual(type(test_dl).__name__, "DeviceDataLoader")
 
+    def test_dataset_ckplus_224_3x3_p1(self):
+        from datasets.pytorch import ckplus as ckplus_pytorch
+        from utils.pytorch import device_management as dm
+        train_dl, _2, _3 = ckplus_pytorch.load_ckplus(
+            self.workdir + "/training_datas/ckplus",
+            dm.get_default_device(),
+            224,
+            batch_size=8,
+            cfg_OnsuNet=False
+        )
+
+        self.assertEqual(type(train_dl).__name__, "DeviceDataLoader")
+
+    def test_dataset_ckplus_224_3x3_p2(self):
+        from datasets.pytorch import ckplus as ckplus_pytorch
+        from utils.pytorch import device_management as dm
+        _1, valid_dl, _3 = ckplus_pytorch.load_ckplus(
+            self.workdir + "/training_datas/ckplus",
+            dm.get_default_device(),
+            224,
+            batch_size=8,
+            cfg_OnsuNet=False
+        )
+
+        self.assertEqual(type(valid_dl).__name__, "DeviceDataLoader")
+
+    def test_dataset_ckplus_224_3x3_p3(self):
+        from datasets.pytorch import ckplus as ckplus_pytorch
+        from utils.pytorch import device_management as dm
+        _1, _2, test_dl = ckplus_pytorch.load_ckplus(
+            self.workdir + "/training_datas/ckplus",
+            dm.get_default_device(),
+            224,
+            batch_size=8,
+            cfg_OnsuNet=False
+        )
+
+        self.assertEqual(type(test_dl).__name__, "DeviceDataLoader")
+
+    def test_dataset_fer2013_224_3x3_p1(self):
+        from datasets.pytorch import fer2013 as fer2013_pytorch
+        from utils.pytorch import device_management as dm
+        train_dl, _2, _3 = fer2013_pytorch.load_fer2013(
+            self.workdir + "/training_datas/fer2013/fer2013.csv",
+            dm.get_default_device(),
+            224,
+            batch_size=8,
+            cfg_OnsuNet=False
+        )
+
+        self.assertEqual(type(train_dl).__name__, "DeviceDataLoader")
+
+    def test_dataset_fer2013_224_3x3_p2(self):
+        from datasets.pytorch import fer2013 as fer2013_pytorch
+        from utils.pytorch import device_management as dm
+        _1, valid_dl, _3 = fer2013_pytorch.load_fer2013(
+            self.workdir + "/training_datas/fer2013/fer2013.csv",
+            dm.get_default_device(),
+            224,
+            batch_size=8,
+            cfg_OnsuNet=False
+        )
+
+        self.assertEqual(type(valid_dl).__name__, "DeviceDataLoader")
+
+    def test_dataset_fer2013_224_3x3_p3(self):
+        from datasets.pytorch import fer2013 as fer2013_pytorch
+        from utils.pytorch import device_management as dm
+        _1, _2, test_dl = fer2013_pytorch.load_fer2013(
+            self.workdir + "/training_datas/fer2013/fer2013.csv",
+            dm.get_default_device(),
+            224,
+            batch_size=8,
+            cfg_OnsuNet=False
+        )
+
+        self.assertEqual(type(test_dl).__name__, "DeviceDataLoader")
+
+    def test_dataset_kdef_224_3x3_p1(self):
+        from datasets.pytorch import kdef as kdef_pytorch
+        from utils.pytorch import device_management as dm
+        train_dl, _2, _3 = kdef_pytorch.load_kdef(
+            self.workdir + "/training_datas/kdef",
+            dm.get_default_device(),
+            224,
+            batch_size=8,
+            cfg_OnsuNet=False
+        )
+
+        self.assertEqual(type(train_dl).__name__, "DeviceDataLoader")
+
+    def test_dataset_kdef_224_3x3_p2(self):
+        from datasets.pytorch import kdef as kdef_pytorch
+        from utils.pytorch import device_management as dm
+        _1, valid_dl, _3 = kdef_pytorch.load_kdef(
+            self.workdir + "/training_datas/kdef",
+            dm.get_default_device(),
+            224,
+            batch_size=8,
+            cfg_OnsuNet=False
+        )
+
+        self.assertEqual(type(valid_dl).__name__, "DeviceDataLoader")
+
+    def test_dataset_kdef_224_3x3_p3(self):
+        from datasets.pytorch import kdef as kdef_pytorch
+        from utils.pytorch import device_management as dm
+        _1, _2, test_dl = kdef_pytorch.load_kdef(
+            self.workdir + "/training_datas/kdef",
+            dm.get_default_device(),
+            224,
+            batch_size=8,
+            cfg_OnsuNet=False
+        )
+
+        self.assertEqual(type(test_dl).__name__, "DeviceDataLoader")
+
 class TensorflowDatasetTest(unittest.TestCase):
     workdir = "D:/emo/appfiles"
 
-    def test_dataset_ckplus_p1(self):
+    def test_dataset_ckplus_48_1x1_p1(self):
         from datasets.tensorflow import ckplus as ckplus_tensorflow
         training_data, _2, _3, _4, _5, _6 = ckplus_tensorflow.load_ckplus(
             self.workdir + "/training_datas/ckplus",
@@ -137,7 +254,7 @@ class TensorflowDatasetTest(unittest.TestCase):
 
         self.assertEqual(type(training_data).__name__, "NumpyArrayIterator")
 
-    def test_dataset_ckplus_p2(self):
+    def test_dataset_ckplus_48_1x1_p2(self):
         from datasets.tensorflow import ckplus as ckplus_tensorflow
         _1, validation_data, _3, _4, _5, _6 = ckplus_tensorflow.load_ckplus(
             self.workdir + "/training_datas/ckplus",
@@ -148,7 +265,7 @@ class TensorflowDatasetTest(unittest.TestCase):
 
         self.assertEqual(type(validation_data).__name__, "NumpyArrayIterator")
 
-    def test_dataset_ckplus_p3(self):
+    def test_dataset_ckplus_48_1x1_p3(self):
         from datasets.tensorflow import ckplus as ckplus_tensorflow
         _1, _2, testing_data, _4, _5, _6 = ckplus_tensorflow.load_ckplus(
             self.workdir + "/training_datas/ckplus",
@@ -159,7 +276,7 @@ class TensorflowDatasetTest(unittest.TestCase):
 
         self.assertEqual(type(testing_data).__name__, "NumpyArrayIterator")
 
-    def test_dataset_fer2013_p1(self):
+    def test_dataset_fer2013_48_1x1_p1(self):
         from datasets.tensorflow import fer2013 as fer2013_tensorflow
         training_data, _2, _3, _4, _5, _6 = fer2013_tensorflow.load_fer2013(
             self.workdir + "/training_datas/fer2013/fer2013.csv",
@@ -170,7 +287,7 @@ class TensorflowDatasetTest(unittest.TestCase):
 
         self.assertEqual(type(training_data).__name__, "NumpyArrayIterator")
 
-    def test_dataset_fer2013_p2(self):
+    def test_dataset_fer2013_48_1x1_p2(self):
         from datasets.tensorflow import fer2013 as fer2013_tensorflow
         _1, validation_data, _3, _4, _5, _6 = fer2013_tensorflow.load_fer2013(
             self.workdir + "/training_datas/fer2013/fer2013.csv",
@@ -181,7 +298,7 @@ class TensorflowDatasetTest(unittest.TestCase):
 
         self.assertEqual(type(validation_data).__name__, "NumpyArrayIterator")
 
-    def test_dataset_fer2013_p3(self):
+    def test_dataset_fer2013_48_1x1_p3(self):
         from datasets.tensorflow import fer2013 as fer2013_tensorflow
         _1, _2, testing_data, _4, _5, _6 = fer2013_tensorflow.load_fer2013(
             self.workdir + "/training_datas/fer2013/fer2013.csv",
@@ -192,7 +309,7 @@ class TensorflowDatasetTest(unittest.TestCase):
 
         self.assertEqual(type(testing_data).__name__, "NumpyArrayIterator")
 
-    def test_dataset_kdef_p1(self):
+    def test_dataset_kdef_48_1x1_p1(self):
         from datasets.tensorflow import kdef as kdef_tensorflow
         training_data, _2, _3, _4, _5, _6 = kdef_tensorflow.load_kdef(
             self.workdir + "/training_datas/kdef",
@@ -203,7 +320,7 @@ class TensorflowDatasetTest(unittest.TestCase):
 
         self.assertEqual(type(training_data).__name__, "NumpyArrayIterator")
 
-    def test_dataset_kdef_p2(self):
+    def test_dataset_kdef_48_1x1_p2(self):
         from datasets.tensorflow import kdef as kdef_tensorflow
         _1, validation_data, _3, _4, _5, _6 = kdef_tensorflow.load_kdef(
             self.workdir + "/training_datas/kdef",
@@ -214,7 +331,7 @@ class TensorflowDatasetTest(unittest.TestCase):
 
         self.assertEqual(type(validation_data).__name__, "NumpyArrayIterator")
 
-    def test_dataset_kdef_p3(self):
+    def test_dataset_kdef_48_1x1_p3(self):
         from datasets.tensorflow import kdef as kdef_tensorflow
         _1, _2, testing_data, _4, _5, _6 = kdef_tensorflow.load_kdef(
             self.workdir + "/training_datas/kdef",
@@ -225,10 +342,114 @@ class TensorflowDatasetTest(unittest.TestCase):
 
         self.assertEqual(type(testing_data).__name__, "NumpyArrayIterator")
 
+    def test_dataset_ckplus_224_3x3_p1(self):
+        from datasets.tensorflow import ckplus as ckplus_tensorflow
+        training_data, _2, _3, _4, _5, _6 = ckplus_tensorflow.load_ckplus(
+            self.workdir + "/training_datas/ckplus",
+            224,
+            batch_size=8,
+            cfg_OnsuNet = False
+        )
+
+        self.assertEqual(type(training_data).__name__, "NumpyArrayIterator")
+
+    def test_dataset_ckplus_224_3x3_p2(self):
+        from datasets.tensorflow import ckplus as ckplus_tensorflow
+        _1, validation_data, _3, _4, _5, _6 = ckplus_tensorflow.load_ckplus(
+            self.workdir + "/training_datas/ckplus",
+            224,
+            batch_size=8,
+            cfg_OnsuNet = False
+        )
+
+        self.assertEqual(type(validation_data).__name__, "NumpyArrayIterator")
+
+    def test_dataset_ckplus_224_3x3_p3(self):
+        from datasets.tensorflow import ckplus as ckplus_tensorflow
+        _1, _2, testing_data, _4, _5, _6 = ckplus_tensorflow.load_ckplus(
+            self.workdir + "/training_datas/ckplus",
+            224,
+            batch_size=8,
+            cfg_OnsuNet = False
+        )
+
+        self.assertEqual(type(testing_data).__name__, "NumpyArrayIterator")
+
+    def test_dataset_fer2013_224_3x3_p1(self):
+        from datasets.tensorflow import fer2013 as fer2013_tensorflow
+        training_data, _2, _3, _4, _5, _6 = fer2013_tensorflow.load_fer2013(
+            self.workdir + "/training_datas/fer2013/fer2013.csv",
+            224,
+            batch_size=8,
+            cfg_OnsuNet = False
+        )
+
+        self.assertEqual(type(training_data).__name__, "NumpyArrayIterator")
+
+    def test_dataset_fer2013_224_3x3_p2(self):
+        from datasets.tensorflow import fer2013 as fer2013_tensorflow
+        _1, validation_data, _3, _4, _5, _6 = fer2013_tensorflow.load_fer2013(
+            self.workdir + "/training_datas/fer2013/fer2013.csv",
+            224,
+            batch_size=8,
+            cfg_OnsuNet = False
+        )
+
+        self.assertEqual(type(validation_data).__name__, "NumpyArrayIterator")
+
+    def test_dataset_fer2013_224_3x3_p3(self):
+        from datasets.tensorflow import fer2013 as fer2013_tensorflow
+        _1, _2, testing_data, _4, _5, _6 = fer2013_tensorflow.load_fer2013(
+            self.workdir + "/training_datas/fer2013/fer2013.csv",
+            224,
+            batch_size=8,
+            cfg_OnsuNet = False
+        )
+
+        self.assertEqual(type(testing_data).__name__, "NumpyArrayIterator")
+
+    def test_dataset_kdef_224_3x3_p1(self):
+        from datasets.tensorflow import kdef as kdef_tensorflow
+        training_data, _2, _3, _4, _5, _6 = kdef_tensorflow.load_kdef(
+            self.workdir + "/training_datas/kdef",
+            224,
+            batch_size=8,
+            cfg_OnsuNet = False
+        )
+
+        self.assertEqual(type(training_data).__name__, "NumpyArrayIterator")
+
+    def test_dataset_kdef_224_3x3_p2(self):
+        from datasets.tensorflow import kdef as kdef_tensorflow
+        _1, validation_data, _3, _4, _5, _6 = kdef_tensorflow.load_kdef(
+            self.workdir + "/training_datas/kdef",
+            224,
+            batch_size=8,
+            cfg_OnsuNet = False
+        )
+
+        self.assertEqual(type(validation_data).__name__, "NumpyArrayIterator")
+
+    def test_dataset_kdef_224_3x3_p3(self):
+        from datasets.tensorflow import kdef as kdef_tensorflow
+        _1, _2, testing_data, _4, _5, _6 = kdef_tensorflow.load_kdef(
+            self.workdir + "/training_datas/kdef",
+            224,
+            batch_size=8,
+            cfg_OnsuNet = False
+        )
+
+        self.assertEqual(type(testing_data).__name__, "NumpyArrayIterator")
+
 class PytorchModelTest(unittest.TestCase):
     def test_model_onsunet(self):
         from models.pytorch import onsunet
-        model = onsunet.Onsunet()
+        model = onsunet.Onsunet(num_classes=7)
+        self.assertEqual(type(model).__name__, "Onsunet")
+
+    def test_model_onsunet(self):
+        from models.pytorch import onsunet
+        model = onsunet.Onsunet(num_classes=1000)
         self.assertEqual(type(model).__name__, "Onsunet")
 
     def test_model_resnet(self):
@@ -236,28 +457,56 @@ class PytorchModelTest(unittest.TestCase):
         model = resnet.resnet50(img_channels=3, num_classes=7)
         self.assertEqual(type(model).__name__, "ResNet")
 
+    def test_model_resnet(self):
+        from models.pytorch import resnet
+        model = resnet.resnet50(img_channels=3, num_classes=1000)
+        self.assertEqual(type(model).__name__, "ResNet")
+
     def test_model_vgg(self):
         from models.pytorch import vgg
         model = vgg.VGG16(num_classes=7)
+        self.assertEqual(type(model).__name__, "VGG16")
+
+    def test_model_vgg(self):
+        from models.pytorch import vgg
+        model = vgg.VGG16(num_classes=1000)
         self.assertEqual(type(model).__name__, "VGG16")
 
 class TensorflowModelTest(unittest.TestCase):
     def test_model_onsunet(self):
         from models.tensorflow import onsunet
         model = onsunet.Onsunet()
-        model.build_model()
+        model.build_model(num_classes=7)
+        self.assertEqual(type(model.model).__name__, "Functional")
+
+    def test_model_onsunet(self):
+        from models.tensorflow import onsunet
+        model = onsunet.Onsunet()
+        model.build_model(num_classes=1000)
         self.assertEqual(type(model.model).__name__, "Functional")
 
     def test_model_resnet(self):
         from models.tensorflow import resnet
         model = resnet.Resnet()
-        model.build_model()
+        model.build_model(num_classes=7)
+        self.assertEqual(type(model.model).__name__, "Functional")
+
+    def test_model_resnet(self):
+        from models.tensorflow import resnet
+        model = resnet.Resnet()
+        model.build_model(num_classes=1000)
         self.assertEqual(type(model.model).__name__, "Functional")
 
     def test_model_vgg(self):
         from models.tensorflow import vgg
         model = vgg.VGG16()
-        model.build_model()
+        model.build_model(num_classes=7)
+        self.assertEqual(type(model.model).__name__, "Functional")
+
+    def test_model_vgg(self):
+        from models.tensorflow import vgg
+        model = vgg.VGG16()
+        model.build_model(num_classes=1000)
         self.assertEqual(type(model.model).__name__, "Functional")
 
 if __name__ == '__main__':
